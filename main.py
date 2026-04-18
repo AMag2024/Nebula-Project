5facf62966c5949552a62a11adce5857e599a95e
Andrei <andreimag2024@gmail.com>
2026-04-17 21:52:49 -0700

    Update main.py
    Base

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import numpy as np
 import hashlib
 
 
+plt.style.use('darl_background')
+fig, ax = plt.subplots()
+
+
 user_input = input("Enter a seed phrase or number: ")
 
+
+#seed system
 hash_bytes = hashlib.md5(user_input.encode()).hexdigest()
 seed = int(hash_bytes, 16) % (2**32)
 print(f"Your phrase '{user_input}' generated seed: {seed}")
